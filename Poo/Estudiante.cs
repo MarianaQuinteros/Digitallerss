9402b40668483900aa890b0a05746a07fb18d343
Ulrich <juyagu@gmail.com>
2021-07-21 21:55:04 -0300

    Encapsulamiento

---
+++ Poo/Estudiante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo
+{
+    class Estudiante
+    {
+        private string dni;
+        private string nombre;
+        private int edad;
+        private string materia;
+        private bool regular;
+
+
+        public void saludar() {
+            Console.WriteLine("Hola yo soy estudiante de Programación");    
+        }
+
+        public void setDni(string dni_param)
+        {
+            dni = dni_param;
+        }
+
+        public string getDni()
+        {
+            return dni;
+        }
+    }
+}
--- Poo/Program.cs
+++ Poo/Program.cs
@@ -10,6 +10,21 @@ namespace Poo
     {
         static void Main(string[] args)
         {
+            // Encapsulamiento establece que los atributos de una clase deben ser privados, y los metodos de acceso públicos
+            //Estudiante e1 = new Estudiante();
+            /*e1.dni = "11456789";
+            e1.nombre = "Maria Perez";
+            e1.edad = 74;
+            e1.materia = "PHP";
+            e1.regular = true;*/
+
+            //Console.WriteLine("El nombre de la estudiante es {0}", e1.nombre);
+
+            Estudiante e2 = new Estudiante();
+            e2.setDni("11456789");
+            e2.saludar();
+            Console.WriteLine("El DNI del estudiante es {0}", e2.getDni());
+            Console.ReadLine();
         }
     }
 }
